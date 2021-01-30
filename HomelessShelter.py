fc23158a780821339a261303611396c2908bab97
nikakp <57926635+nikakp@users.noreply.github.com>
2021-01-30 18:12:51 -0500

    Add files via upload
    updated html with jinja2

--- HomelessShelter.py
+++ HomelessShelter.py
@@ -1,21 +1,25 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
-def main_page():
-    return 'Homeless Shelters'
+def home():
 
-@app.route('/Shelter')
-def shelter():
-    return render_template("shelterAdmin.html")
+    # Opening JSON file with homesless shelters info
+    f = open('shelters.json')
 
-@app.route('/login', methods=["GET","POST"])
-def login():
-    if request.method == 'POST':
-        print(request.form)
-    return render_template("login.html")
+    #returns JSON object as a dictionary
+    shelters_data = json.load(f)
 
-app.run('0.0.0.0', 8000, debug = True)
+
+    for i in shelters_data['shelters']:
+        print(i['name'])
+
+    # Closing file
+    f.close()
+
+    return render_template('index.html', shelters_data=shelters_data)
+if __name__ == '__main__':
+    app.run()
+#app.run('0.0.0.0', 8000, debug = True)
